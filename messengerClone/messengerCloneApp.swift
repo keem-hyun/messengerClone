bce7c04df84079a45ad177ef7e6c161cd141040e
keem-hyun <hw813@naver.com>
2024-06-15 16:27:51 +0900

    init

---
+++ messengerClone/messengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  messengerCloneApp.swift
+//  messengerClone
+//
+//  Created by 김강현 on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct messengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
